c28fae6653501f33ee4d87ba679c9acc3221de52
summer <yaolei313@users.noreply.github.com>
2017-06-22 00:49:06 +0800

    fix

--- base/stat.py
+++ base/stat.py
@@ -1,3 +1,8 @@
+import logging
+import traceback
+import math
+
+
 def normalize(s):
     """
     Convert s to a normalized string
@@ -38,6 +43,8 @@ def stat_file():
     d = make_freq_stat(body)
     s1 = sum(d[k] for k in d)
     s2 = len(d.keys())
+    print('%s %s %s' % (num_chars, s1, s2))
+
     lst = [(d[w], w) for w in d]
     lst.sort()
     lst.reverse()
@@ -47,7 +54,65 @@ def stat_file():
         i += 1
 
 
+def stat_file_line():
+    num_chars = 0
+    d = {}
+    with open('E:\download\The Life Story of an Otter.txt', 'r', encoding='utf-8') as fd:
+        for line in fd:
+            num_chars += len(line)
+            t_d = make_freq_stat(line)
+            for item in t_d:
+                if item in d:
+                    d[item] += t_d[item]
+                else:
+                    d[item] = t_d[item]
+    s1 = sum(d[k] for k in d)
+    s2 = len(d.keys())
+    print('{} {} {}', (num_chars, s1, s2))
+    lst = [(d[w], w) for w in d]
+    lst.sort()
+    lst.reverse()
+    i = 1
+    for (count, word) in lst[:10]:
+        print('%2s %4s %s' % (i, count, word))
+        i += 1
+
+
+def ask_retry(prompt, retries=2, reminder="please try again"):
+    while True:
+        ok = input(prompt).lower()
+        if ok in ('y', 'yes'):
+            return True
+        elif ok in ('n', 'no'):
+            return False
+        retries = retries - 1
+        if retries < 0:
+            raise ValueError('invalid user response')
+        print(reminder)
+
+
+def f(a, L=None):
+    if L is None:
+        L = []
+    L.append(a)
+    return L
+
+
+print(f(0))
+print(f(1))
+print(f(2))
+print('-------------')
 str1 = "hello world,li bai!"
 print(normalize(str1))
 
 stat_file()
+print('-------------')
+# stat_file_line()
+# print('-------------')
+# try:
+#     ask_retry("input y to end")
+# except ValueError as e:
+#     # logging.error(e)
+#     logging.exception("error message")
+
+list(range(3,10))
